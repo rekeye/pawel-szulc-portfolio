f58aac83ea22e9940f062dd989aa8307c0a9e86e
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-09-17 11:51:30 +0200

    initial package installations/ adding plugins to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,16 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "Project",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "Project",
+        // Url to query from
+        url: "https://api-eu-central-1.graphcms.com/v2/ckto5qx0u0tfm01yzg1j4a85i/master",
+      },
+    },
   ],
 };
