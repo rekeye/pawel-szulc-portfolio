29a12fa969fcc297a94b3d7d0f9ce766deb7f4e9
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-09-19 14:11:48 +0200

    adding files referencing - acb5a78 ('project element generated from graphcms', 2021-09-19)

---
+++ src/components/Project.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+const Project = ({ data: { title, slug, dateOfCreation, image } }) => {
+  const imageData = getImage(image.localFile);
+  console.log(imageData);
+  return (
+    <Link className='project' to={`/projects/${slug}`}>
+      <article>
+        <GatsbyImage image={imageData} alt={title} />
+        <p>{`${title}, ${dateOfCreation}`}</p>
+      </article>
+    </Link>
+  );
+};
+
+export default Project;
